6e37c188f48043dcaaa6ff0ab791c874eab5162f
rahul <rahulkumar@incture.com>
2024-07-18 19:47:04 +0530

    Initial project setup, setup both frontend and backend, installed tailwind and express

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+   return (
+      <>
+         <div className='flex min-h-screen justify-center items-center text-8xl font-bold text-orange-600'>
+            EasyGrocers
+         </div>
+      </>
+   )
+}
+
+export default App

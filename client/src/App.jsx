bf4ad7f807ed99ceb345a342646ec5ce63ffb025
Rahul Choudhary <rahulchoudhary0775@gmail.com>
2024-07-18 20:00:43 +0530

    checking account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
    return (
       <>
          <div className='flex min-h-screen justify-center items-center text-8xl font-bold text-orange-600'>
-            EasyGrocers
+            EasyGrocers : Get your groceries delivered at home.
          </div>
       </>
    )
